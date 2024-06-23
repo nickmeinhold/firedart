fbc1ddd6314289a8cc3bf0463d55579baf2b0725
Nick Meinhold <nick@enspyr.co>
2024-06-24 09:46:09 +1000

    Document token_authenticator.dart

--- lib/firestore/token_authenticator.dart
+++ lib/firestore/token_authenticator.dart
@@ -1,5 +1,10 @@
 import '../firedart.dart';
 
+/// The [TokenAuthenticator] keeps a [FirebaseAuth] member, which is passed in
+/// during [Firestore.initialize] when not using Application Default Credentials.
+///
+/// The [authenticate] function is passed to [Firestore]'s [FirestoreGateway]
+/// where it is used to authenticate requests.
 class TokenAuthenticator {
   final FirebaseAuth auth;
 
