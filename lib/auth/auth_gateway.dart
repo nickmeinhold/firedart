0d342140ba75294f080a80245391bda671446b52
Nick Meinhold <nick@enspyr.co>
2024-06-24 10:57:33 +1000

    Doument auth_gateway.dart

--- lib/auth/auth_gateway.dart
+++ lib/auth/auth_gateway.dart
@@ -6,25 +6,35 @@ import 'package:firedart/auth/token_provider.dart';
 import 'exceptions.dart';
 import 'user_gateway.dart';
 
+/// Keeps a [KeyClient] and [TokenProvider] to allow interacting with the
+/// FirebaseAuth API.
+///
+/// On authenticating a [User] object is returned.
 class AuthGateway {
   final KeyClient client;
   final TokenProvider tokenProvider;
 
   AuthGateway(this.client, this.tokenProvider);
 
+  /// Create a new email and password user with the signupNewUser endpoint.
   Future<User> signUp(String email, String password) =>
       _auth('signUp', {'email': email, 'password': password})
           .then(User.fromMap);
 
+  /// Sign in a user with an email and password using the verifyPassword endpoint.
   Future<User> signIn(String email, String password) =>
       _auth('signInWithPassword', {'email': email, 'password': password})
           .then(User.fromMap);
 
+  /// Exchange a custom Auth token for an ID and refresh token using the
+  /// verifyCustomToken endpoint.
   Future<void> signInWithCustomToken(String token) => _auth(
       'signInWithCustomToken', {'token': token, 'returnSecureToken': 'true'});
 
+  /// Sign in a user anonymously using the signupNewUser endpoint.
   Future<User> signInAnonymously() => _auth('signUp', {}).then(User.fromMap);
 
+  /// Apply a password reset change using the resetPassword endpoint.
   Future<void> resetPassword(String email) => _post('sendOobCode', {
         'requestType': 'PASSWORD_RESET',
         'email': email,
