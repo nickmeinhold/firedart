762f7c3460a451d1fc06cc3ae7196cdd32d87117
Nick Meinhold <nick@enspyr.co>
2024-06-24 11:07:02 +1000

    Document exceptions.dart

--- lib/auth/exceptions.dart
+++ lib/auth/exceptions.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
 
+/// An [Exception] that includes the body of a http request response.
 class AuthException implements Exception {
   final String body;
 
+  /// Extract the error message from the body.
   String get message => jsonDecode(body)['error']['message'];
 
+  /// Extract the error code from the error message.
   String get errorCode => message.split(' ')[0];
 
   AuthException(this.body);
@@ -13,6 +16,8 @@ class AuthException implements Exception {
   String toString() => 'AuthException: $errorCode';
 }
 
+/// An [Exception] thrown when an app attempted to make a Firestore request
+/// before the user is signed in.
 class SignedOutException implements Exception {
   @override
   String toString() =>
